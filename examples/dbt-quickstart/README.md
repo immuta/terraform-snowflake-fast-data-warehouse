85428f9a21027a4371860d95976a8c8d40083cb4
Stephen Bailey <stkbailey@gmail.com>
2021-03-16 00:13:45 -0400

    Add examples

---
+++ examples/dbt-quickstart/README.md
@@ -0,0 +1,4 @@
+# Fishtown Analyltics Quick-Start
+
+Instantiating the above module will create a set of resources mostly aligned
+with those outined in [this great Fishtown Analytics blog post](https://blog.getdbt.com/how-we-configure-snowflake/).
---
+++ examples/immuta-basic/employees.tf
@@ -0,0 +1,20 @@
+locals {
+  employees = {
+    employeeA = {
+      name       = "employeeA"
+      login_name = "employeeA@immuta.com"
+    }
+    employeeB = {
+      name       = "employeeB"
+      login_name = "employeeB@immuta.com"
+    }
+    employeeC = {
+      name       = "employeeC"
+      login_name = "employeeC@immuta.com"
+    }
+    employeeD = {
+      name       = "employeeD"
+      login_name = "employeeD@immuta.com"
+    }
+  }
+}
