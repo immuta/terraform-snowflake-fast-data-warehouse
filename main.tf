faf656771e9bfb25daf4e0d8cf275c7dfcd99a18
Stephen Bailey <stkbailey@gmail.com>
2021-03-25 06:20:54 -0400

    add example users

--- main.tf
+++ main.tf
@@ -9,25 +9,13 @@ terraform {
 
 // This module generates base roles, warehouses and users
 // It does NOT create grants between these users
-
-module "core" {
+module "example_core" {
   source = "./modules/core"
 
-  employee_users = {
-    "dev1" = { name = "dev1", login_name = "dev1@immuta.com" }
-    "dev2" = { name = "dev2", login_name = "dev2@immuta.com" }
-    "consumer1" = { name = "consumer", login_name = "consumer@immuta.com" }
-  }
-  system_users = {
-    "dbt"    = { name = "DBT" }
-    "immuta" = { name = "IMMUTA" }
-    "meltano" = { name = "MELTANO" }
-  }
+  employee_users = {"sbailey" = { name = "SBAILEY" } }
+  system_users = {"immuta"  = { name = "IMMUTA" }}
   roles = {
-    loader      = { name = "LOADER" }
-    transformer = { name = "TRANSFORMER" }
-    reporter      = { name = "REPORTER" }
-
+    analyst    = { name = "REPORTER" }
   }
   warehouses = {
     transform = { name = "TRANSFORM_WH" }
@@ -36,35 +24,13 @@ module "core" {
 }
 
 // databases
-module "analytics_db" {
+module "example_db" {
   source = "./modules/app_database"
 
-  db_name     = "ANALYTICS"
+  db_name             = "ANALYTICS"
   grant_role_to_roles = ["SYSADMIN"]
   grant_role_to_users = [module.core.system_users["dbt"].name]
   grant_read_to_roles = [module.core.roles["reporter"].name]
-}
-
-module "raw_db" {
-  source = "./modules/app_database"
-
-  db_name                = "RAW"
-  grant_role_to_roles    = ["SYSADMIN"]
-  grant_role_to_users = [module.core.system_users["meltano"].name]
-  grant_read_to_roles    = [
-    module.core.roles["reporter"].name,
-    module.core.roles["transformer"].name,
-  ]
-}
-
-module "developer_dbs" {
-  for_each = ["dev1", "dev2"]
-  source   = "./modules/app_database"
-
-  db_name             = each.value
-  create_user         = false
-  create_warehouse    = false
-  grant_role_to_users = [each.key]
 }
 
 // role and warehouse grants
@@ -83,7 +49,7 @@ resource "snowflake_warehouse_grant" "transform" {
     [for m in module.developer_dbs : m.role.name],
     [module.analytics_db.role.name]
   )
-)
+}
 
 resource "snowflake_warehouse_grant" "report" {
   warehouse_name = module.core.warehouses["report"].name
