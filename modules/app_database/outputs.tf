6c5bcbcc18b5cafad51f4241c20f88aef6e65af9
Stephen Bailey <stkbailey@gmail.com>
2021-03-25 06:45:34 -0400

    Fix description typo

--- modules/app_database/outputs.tf
+++ modules/app_database/outputs.tf
@@ -9,11 +9,11 @@ output "role" {
 }
 
 output "user" {
-  desription = "User resource created for the app. May be null."
+  description = "User resource created for the app. May be null."
   value      = var.create_user ? snowflake_user.app[0] : null
 }
 
 output "warehouse" {
-  desription = "Warehouse resource created for the app. May be null."
+  description = "Warehouse resource created for the app. May be null."
   value      = var.create_warehouse ? snowflake_warehouse.app[0] : null
 }
