513cc8b7d2e22368968fb42fab6df01517f39dfe
Kyle Lundstedt <klundstedt@industryvault.com>
2021-11-17 14:22:56 -0800

    create analytics only

--- main.tf
+++ main.tf
@@ -7,91 +7,91 @@ provider "snowflake" {
 }
 
 locals {
-  developer_list = ["harry", "hermione"]
+  developer_list = ["klundstedt", "akillinger"]
 }
 
-module "employees" {
-  source = "./modules/bulk_users"
-  users = {
-    "harry" = {}
-    "ron" = {
-      first_name = "Ronald"
-    }
-    "hermione" = {}
-    "fred" = {
-      login_name = "fred"
-    }
-  }
+# module "employees" {
+#   source = "./modules/bulk_users"
+#   users = {
+#     "harry" = {}
+#     "ron" = {
+#       first_name = "Ronald"
+#     }
+#     "hermione" = {}
+#     "fred" = {
+#       login_name = "fred"
+#     }
+#   }
 
-  default_role                   = "PUBLIC"
-  default_generate_user_password = true
-}
+#   default_role                   = "PUBLIC"
+#   default_generate_user_password = true
+# }
 
-module "bulk_roles" {
-  source = "./modules/bulk_roles"
-  roles = {
-    analyst = { name = "ANALYST_ROLE" }
-  }
-}
+# module "bulk_roles" {
+#   source = "./modules/bulk_roles"
+#   roles = {
+#     analyst = { name = "ANALYST_ROLE" }
+#   }
+# }
 
-module "bulk_warehouses" {
-  source = "./modules/bulk_warehouses"
-  warehouses = {
-    transform = {
-      name                    = "TRANSFORM_WH"
-      create_resource_monitor = true
-    }
-    report = {
-      name = "REPORTING_WH"
-      size = "medium"
-    }
-  }
-  default_size    = "x-small"
-  default_comment = "This is my warehouse comment."
-}
+# module "bulk_warehouses" {
+#   source = "./modules/bulk_warehouses"
+#   warehouses = {
+#     transform = {
+#       name                    = "TRANSFORM_WH"
+#       create_resource_monitor = true
+#     }
+#     report = {
+#       name = "REPORTING_WH"
+#       size = "medium"
+#     }
+#   }
+#   default_size    = "x-small"
+#   default_comment = "This is my warehouse comment."
+# }
 
-// role and warehouse grants
-module "bulk_role_grants" {
-  source = "./modules/bulk_role_grants"
-  grants = {
-    analyst = {
-      role_name = module.bulk_roles.roles["analyst"].name
-      users     = [module.employees.users["harry"].name]
-    }
-  }
-}
+# // role and warehouse grants
+# module "bulk_role_grants" {
+#   source = "./modules/bulk_role_grants"
+#   grants = {
+#     analyst = {
+#       role_name = module.bulk_roles.roles["analyst"].name
+#       users     = [module.employees.users["harry"].name]
+#     }
+#   }
+# }
 
-module "bulk_warehouse_grants" {
-  source = "./modules/bulk_warehouse_grants"
-  grants = {
-    transform = {
-      warehouse_name = module.bulk_warehouses.warehouses["transform"].name
-      roles          = [module.bulk_roles.roles["analyst"].name]
-    }
-    report = {
-      warehouse_name = module.bulk_warehouses.warehouses["report"].name
-      roles          = [module.bulk_roles.roles["analyst"].name]
-    }
-  }
-}
+# module "bulk_warehouse_grants" {
+#   source = "./modules/bulk_warehouse_grants"
+#   grants = {
+#     transform = {
+#       warehouse_name = module.bulk_warehouses.warehouses["transform"].name
+#       roles          = [module.bulk_roles.roles["analyst"].name]
+#     }
+#     report = {
+#       warehouse_name = module.bulk_warehouses.warehouses["report"].name
+#       roles          = [module.bulk_roles.roles["analyst"].name]
+#     }
+#   }
+# }
 
 // databases
 module "example_db" {
   source = "./modules/application_database"
 
   database_name        = "ANALYTICS"
-  grant_admin_to_roles = []
-  grant_admin_to_users = [module.employees.users["ron"].name]
-  grant_read_to_roles  = [module.bulk_roles.roles["analyst"].name]
+  # grant_admin_to_roles = []
+  # grant_admin_to_users = [module.employees.users["ron"].name]
+  # grant_read_to_roles  = [module.bulk_roles.roles["analyst"].name]
 }
 
-module "developer_dbs" {
-  for_each = toset(local.developer_list)
-  source   = "./modules/application_database"
+# module "developer_dbs" {
+#   for_each = toset(local.developer_list)
+#   source   = "./modules/application_database"
 
-  database_name                = "DEV_${module.employees.users[each.key].name}"
-  admin_role_name_suffix       = ""
-  create_application_user      = false
-  create_application_warehouse = false
-  grant_admin_to_users         = [module.employees.users[each.key].name]
-}
+#   database_name                = "DEV_${module.employees.users[each.key].name}"
+#   admin_role_name_suffix       = ""
+#   create_application_user      = false
+#   create_application_warehouse = false
+#   grant_admin_to_users         = [module.employees.users[each.key].name]
+# }
